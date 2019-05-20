6e6f8d530bd9da6ece4338bb994608dbe9367a74
adityaaggarwal1992 <aggaraditya@gmail.com>
2019-05-20 14:40:40 -0500

    ACK: acknowledged comments and tried resolving the remaining ones.

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
-//Your name with the command line arguments as the application assumes the first string as the name of the user.
-//
-//The class Program is the main class encapsulating the main method.
+///Your name with the command line arguments as the application assumes the first string as the name of the user.
+///
+///The class Program is the main class encapsulating the main method.
 
 namespace GradeBook
 {
@@ -11,11 +11,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            string username;
             var gradeBookName = "first";
-            var book = new Book(gradeBookName, args);
-            book.WelcomeUser(args);
+            username = args.Length<1 ? "" : args[0];
+            var book = new Book(gradeBookName, username);
             AddShowStats(book);
-            book.ShowStatistics();
         }
 
         public static void AddShowStats(Book book)
@@ -24,6 +24,8 @@ namespace GradeBook
             book.ShowStatistics();
             Random random = new Random();
             book.AddGrade(random.Next(0,100));
+            System.Console.WriteLine("\nAfter adding the grade:");
+            book.ShowStatistics();
         } 
     }
 }
--- gradebook/src/GradeBook/book.cs
+++ gradebook/src/GradeBook/book.cs
@@ -1,31 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
-
-// The class Book packages the blueprint book provides ways to:
-// 1 store the grades. 
-// 2 add to the given gradebook obj
-// 3 welcomes the user from args
-// 4 Assign name to the gradebook
+/// The class Book packages the blueprint book provides ways to:
+/// 1 store the grades. 
+/// 2 add to the given gradebook obj
+/// 3 welcomes the user from args
+/// 4 Assign name to the gradebook
 
 
 namespace GradeBook
 {
     public class Book
     {
-        private List<double> grades;
+        private List <double> grades;
         private string name;
         private double highGrade = double.MinValue;
         private double lowGrade = double.MaxValue;
         private double avg = 0.0;
 
-        //Constructor
-        public Book(string name, string[] args)
+        ///Constructor
+        /// User for initializing the members
+        public Book(string name, string username)
         {
+            WelcomeUser(username);
             grades = new List<double>() { 1, 22, 56, 33.2, 45, 30, 32 };
             this.name = name;
         }
 
+        /// calculate statistics such as high, low and avg grade
         public void CalculateStats()
         {
             foreach (double gd in this.grades)
@@ -37,32 +40,27 @@ namespace GradeBook
             avg /= this.grades.Count;
         }
 
-        // show the current statistics
+        /// show the current statistics
         public void ShowStatistics()
         {
             CalculateStats();
             Console.WriteLine($"Min grade - {lowGrade}\nMax Grade- {highGrade}\nAvg Grade - {avg}");
         }
 
-        // Welcomes the user
-        public void WelcomeUser(string[] args)
+        /// Welcomes the user
+        public void WelcomeUser(string name)
         {
-            string user = "";
-            if (args.Length > 0)
+            if (string.IsNullOrWhiteSpace(name))
             {
-                user = args[0];
-            }
-            if (string.IsNullOrWhiteSpace(user))
-            {
-                throw new ArgumentException("the argument was null or white space");
+                throw new ArgumentException("The username is either whitespice or blank. Expecting an alphanumeric string w/o whitespace.");
             } 
             else
             {
-                Console.WriteLine($"Welcome, {user}");
+                Console.WriteLine($"Welcome, {name}");
             }
         }
 
-        // Adds grade to the class member
+        /// Adds grade to the class member
         public void AddGrade(double grade)
         {
             if((grade> 0) && (grade < 101))
@@ -71,10 +69,14 @@ namespace GradeBook
             }
         }
 
-        // getter for grades member
-        public List<double> GetGrade()
+        /// getter for grades member
+        public ReadOnlyCollection<double> readOnlyGrades
         {
-            return grades;
+            get
+            {
+                ReadOnlyCollection<double> readOnlyGrades = new ReadOnlyCollection<double>(grades);
+                return readOnlyGrades;
+            }
         }
     }
 }
